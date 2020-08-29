7a567b68f9cd7f741cd5c5e339416986f7f2fdae
soroushby <49848393+soroushby@users.noreply.github.com>
2020-08-29 02:04:32 -0700

    added login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
+import { AuthService } from 'src/app/services/auth.service';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'soccerPodcast2';
+
+  constructor(public auth: AuthService) {}
 }
--- src/app/episodes/episode-content/episode-content.component.ts
+++ src/app/episodes/episode-content/episode-content.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Episodes } from 'src/app/interfaces/episodes';
+import { DataService } from 'src/app/services/data.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-episode-content',
   templateUrl: './episode-content.component.html',
-  styleUrls: ['./episode-content.component.scss']
+  styleUrls: ['./episode-content.component.scss'],
 })
 export class EpisodeContentComponent implements OnInit {
-
-  constructor() { }
+  @Input() episode: Episodes;
+  constructor(
+    private dataService: DataService,
+    private sanitizer: DomSanitizer
+  ) {}
 
   ngOnInit(): void {
+    console.log(this.episode.instagram);
   }
+  onDeleteEpisodes(id) {
+    //return this.dataService.deleteEpisodes(id);
+  }
+  sendUrl(url: string): SafeResourceUrl {
+    // const lastChar = url.substr(url.length - 1);
+    // if (url !== '/') url += '/';
+    const trustedDashboardUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
+      url
+    );
 
+    return trustedDashboardUrl;
+  }
 }
